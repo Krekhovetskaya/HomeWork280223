d77a7a0553a35b603b804ee7211fead795c180f3
Elizaveta Krekhovetskaya <okinaliza@gmail.com>
2023-03-01 01:01:31 +0300

    Задача про поиск четных чисел в массиве

---
+++ Task34/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+Console.Clear();
+int[] startArray = GetArray(4, 100 , 999);
+Console.WriteLine(String.Join(" ", startArray));
+Console.WriteLine($"Количество четных элементов в массиве [100; 1000] = {GetCountElements(startArray, 100, 1000)}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(100, 1000);
+    }
+    return res;
+}
+
+int GetCountElements(int[] array, int leftRange, int rigthRange)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
